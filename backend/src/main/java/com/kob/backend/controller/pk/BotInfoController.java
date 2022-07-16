0c380db4ac97f6080d134704ae788656950e8b17
wang bingbing <2669380353@qq.com>
2022-07-16 23:07:42 +0800

    create_backend

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,36 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/***
+ * @Autor: 王冰冰
+ * @Date: 2022/7/16 22:51
+ *
+ */
+@RestController
+@RequestMapping("/Pk")
+public class BotInfoController {
+
+    @RequestMapping("/getBotInfo")
+    public List<String> getBotInfo(){
+        List<String> list = new ArrayList<>();
+        list.add("WangBb");
+        list.add("tiger");
+        list.add("apple");
+        return list;
+    }
+
+    @RequestMapping("/getDictionary")
+    public Map<String, String> getDictionary() {
+        Map<String, String> map = new HashMap<>();
+        map.put("WangBb", "666666");
+        map.put("Tiger", "520");
+        return map;
+    }
+}
