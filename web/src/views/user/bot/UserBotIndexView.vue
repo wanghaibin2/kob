c497a91b3b1634db5069e31db49684636d84bef8
wanghb2 <wanghb2@feifanuniv.com>
2022-09-15 00:42:26 +0800

    0915web

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -3,13 +3,88 @@
 </template>
 
 <script>
-import ContentFiled from '../../../components/ContentFiled.vue'
+    import ContentFiled from '../../../components/ContentFiled.vue'
+    import $ from 'jquery'
+    import { useStore } from 'vuex'
 
-export default {
-    components: {
-        ContentFiled
+    export default {
+        components: {
+            ContentFiled
+        },
+        setup() {
+            const store = useStore();
+            // $.ajax({
+            //     url: "http://127.0.0.1:3000/user/bot/add",
+            //     type: "post",
+            //     data: {
+            //         title: "Bot的title",
+            //         description: "Bot的description",
+            //         context: "Bot的context",
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     }
+            // })
+
+            // $.ajax({
+            //     url: "http://127.0.0.1:3000/user/bot/remove",
+            //     type: "post",
+            //     data: {
+            //         botId: 3,
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     }
+            // })
+            
+            // $.ajax({
+            //     url: "http://127.0.0.1:3000/user/bot/update",
+            //     type: "post",
+            //     data: {
+            //         botId: 2,
+            //         title: "Bot的title002",
+            //         description: "Bot的description002",
+            //         context: "Bot的context002",
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     }
+            // })
+
+            $.ajax({
+                url: "http://127.0.0.1:3000/user/bot/search",
+                type: "get",
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    console.log(resp);
+                },
+                error(resp) {
+                    console.log(resp);
+                }
+            })
+
+        }
     }
-}
 </script>
 
 <style scoped>
